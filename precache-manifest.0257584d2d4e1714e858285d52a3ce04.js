f1ec304e28786df03065ae0e00f7e273cec450fc
白夜 <10874031+zc-up@user.noreply.gitee.com>
2022-04-29 23:26:44 +0800

    deploy

---
+++ precache-manifest.0257584d2d4e1714e858285d52a3ce04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee541b85d28ac954b4a2",
+    "url": "/accounting-1-website/css/app.4fbc2a96.css"
+  },
+  {
+    "revision": "8022408b36bf75b765ad9a7c9d34541a",
+    "url": "/accounting-1-website/index.html"
+  },
+  {
+    "revision": "ee541b85d28ac954b4a2",
+    "url": "/accounting-1-website/js/app.3242e366.js"
+  },
+  {
+    "revision": "d5618ba6fa3dd6801eae",
+    "url": "/accounting-1-website/js/chunk-vendors.5c8c75bf.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/accounting-1-website/manifest.json"
+  },
+  {
+    "revision": "a938a892247a28bd81942520191a0c6b",
+    "url": "/accounting-1-website/qq.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accounting-1-website/robots.txt"
+  }
+]);
